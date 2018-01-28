fc70629a3cbb6d065421906cc77194393c1206ab
KlwntSingh <kulwantbughipura@gmail.com>
2018-01-28 08:18:50 -0400

    working file encrypter implemented

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
 #!venv/bin/python
 import sys
 import os
+from encrypter import encrypt_file as ef
 
 def help():
     print("eu usage:")
     print("eu init - to initialize current directory as encrypt upload repository")
     print()
 
+def call(arg, cb, key=False):
+    index = 1
+    if sys.argv[index] == arg :
+        if key == True:
+            print(sys.argv)
+            key_val = input("Enter the key")
+            cb(key_val, ",".join(sys.argv[index + 1: ]))
+        else:
+            cb(sys.argv[index + 1:])
+        sys.exit(0)
+
+
 def initializer():
     os.makedirs(".eu")
 
 def check_if_eu_dir():
-    return os._exists(".eu")
+    return os.path.exists(".eu")
 
 def args_decision_maker(argv):
     opts = {}  # Empty dictionary to store key-value pairs.
@@ -25,7 +38,9 @@ def args_decision_maker(argv):
         initializer()
     else:
         if check_if_eu_dir():
-            pass
+
+            call("encrypt", ef, key=True)
+
         else:
             sys.stderr.write("not a eu repository")
             sys.exit(1)
--- encrypter.py
+++ encrypter.py
@@ -1,8 +1,8 @@
+import hashlib
 import os, random, struct
 from Crypto.Cipher import AES
 
-
-def encrypt_file(key, in_filename, out_filename=None, chunksize=64*1024):
+def encrypt_file(key, *in_filenames):
     """ Encrypts a file using AES (CBC mode) with the
         given key.
 
@@ -23,23 +23,27 @@ def encrypt_file(key, in_filename, out_filename=None, chunksize=64*1024):
             sizes can be faster for some files and machines.
             chunksize must be divisible by 16.
     """
-    if not out_filename:
+    chunksize = 64 * 1024
+    key = hashlib.sha256(key.encode("utf-8")).digest()
+
+    for in_filename in in_filenames:
         out_filename = in_filename + '.enc'
 
-    iv = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
-    encryptor = AES.new(key, AES.MODE_CBC, iv)
-    filesize = os.path.getsize(in_filename)
+        iv = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
 
-    with open(in_filename, 'rb') as infile:
-        with open(out_filename, 'wb') as outfile:
-            outfile.write(struct.pack('<Q', filesize))
-            outfile.write(iv)
+        encryptor = AES.new(key, AES.MODE_CBC, iv)
+        filesize = os.path.getsize(in_filename)
 
-            while True:
-                chunk = infile.read(chunksize)
-                if len(chunk) == 0:
-                    break
-                elif len(chunk) % 16 != 0:
-                    chunk += ' ' * (16 - len(chunk) % 16)
+        with open(in_filename, 'rb') as infile:
+            with open(out_filename, 'wb') as outfile:
+                outfile.write(struct.pack('<Q', filesize))
+                outfile.write(iv)
 
-                outfile.write(encryptor.encrypt(chunk))
+                while True:
+                    chunk = infile.read(chunksize)
+                    if len(chunk) == 0:
+                        break
+                    elif len(chunk) % 16 != 0:
+                        chunk += ' ' * (16 - len(chunk) % 16)
+
+                    outfile.write(encryptor.encrypt(chunk))
